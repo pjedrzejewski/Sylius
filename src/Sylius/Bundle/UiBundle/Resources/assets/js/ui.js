bdbe843525b75cf8c2c12470b2cb739a99cda160
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2014-07-02 18:33:59 +0200

    Add new theme with responsive dashboard

--- src/Sylius/Bundle/UiBundle/Resources/assets/js/ui.js
+++ src/Sylius/Bundle/UiBundle/Resources/assets/js/ui.js
@@ -6,18 +6,42 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-(function ( $ ) {
+(function ($) {
     'use strict';
 
+    var mobileView = 992;
+
+    function adjustSidebar() {
+        if (window.innerWidth >= mobileView) {
+            if ($.cookie('toggle') === undefined) {
+                $('#page-wrapper').addClass('active');
+            } else {
+                if($.cookie('toggle') == 'true') {
+                    $('#page-wrapper').addClass('active');
+                } else {
+                    $('#page-wrapper').removeClass('active');
+                }
+            }
+        } else {
+            $('#page-wrapper').removeClass('active');
+        }
+    };
+
+    $(window).resize(function() {
+        adjustSidebar();
+    });
+
     $(document).ready(function() {
-        $('.variant-table-toggle i.glyphicon').on('click', function(e) {
-            $(this).toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
-            $(this).parent().parent().find('table tbody').toggle();
+        adjustSidebar();
+
+        $('#sidebar-toggle').click(function(e) {
+            e.preventDefault();
+            $('#page-wrapper').toggleClass('active');
+
+            $.cookie('toggle', $('#page-wrapper').hasClass('active'));
         });
 
-        $('#content-container').css('min-height', $(window).height());
+        $('.dropdown-toggle').dropdown()
+        $('[rel="tooltip"]').tooltip();
     });
-    $(window).resize(function() {
-        $('#content-container').css('min-height', $(window).height());
-    });
-})( jQuery );
+})(jQuery);
