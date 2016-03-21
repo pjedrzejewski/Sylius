9c906386ebe7c6fbed98d7e741288b4be8112231
Kamil Kokot <kamil@kokot.me>
2016-03-21 11:10:01 +0100

    [Resource] Added StateMachineBundle to ThemeBundle functional tests

--- src/Sylius/Bundle/ThemeBundle/Tests/Functional/app/DefaultTestCase/bundles.php
+++ src/Sylius/Bundle/ThemeBundle/Tests/Functional/app/DefaultTestCase/bundles.php
@@ -14,6 +14,7 @@ return [
 
     new FOS\RestBundle\FOSRestBundle(),
     new JMS\SerializerBundle\JMSSerializerBundle(),
+    new \winzou\Bundle\StateMachineBundle\winzouStateMachineBundle(),
     new Sylius\Bundle\ResourceBundle\SyliusResourceBundle(),
     new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
     new Symfony\Bundle\TwigBundle\TwigBundle(),
